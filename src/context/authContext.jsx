2624f0579b3d207ac02ffecf2672ea36cce9504b
Gaurika Bhatt <gaurikabhatt27@gmail.com>
2026-02-20 04:23:27 +0530

    feat: implement auth context for managing user state

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,32 +1,33 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import {getCurrentUser} from "../api/authApi";
+import { getCurrentUser } from "../api/authApi";
 
 const AuthContext = createContext();
 
-export const AuthProvider = ({children}) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] =useState(true);
+export const AuthProvider = ({ children }) => {
 
-    useEffect(() => {
-        const initialize = async () => {
-            try{
-                const data = await getCurrentUser();
-                setUser(data);
-            }catch{
-                setUser(null);
-            }finally{
-                setLoading(false);
-            }
-        };
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-        initialize();
-    }, []);
+  useEffect(() => {
+    const initialize = async () => {
+      try {
+        const data = await getCurrentUser();
+        setUser(data);
+      } catch {
+        setUser(null);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    return(
-        <AuthContext.Provider value = {{user, setUser, loading}}>
-            {children}
-        </AuthContext.Provider>
-    );
+    initialize();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => useContext(AuthContext);
