5e872f54e661e089e45ae9ecce24f390db274d3d
Gaurika Bhatt <gaurikabhatt27@gmail.com>
2026-02-20 04:15:32 +0530

    feat: configure base React app and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react';
-import './App.css';
-import { BrowserRouter } from BrowserRouter;
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import { AuthProvider } from "./context/AuthContext";
+import Layout from "./components/Layout";
+import { Toaster } from "react-hot-toast";
+import VerifyOtp from "./pages/Verifyotp";
 
 function App() {
-  <BrowserRouter>
-  <AuthProvider>
-      
-
-  </AuthProvider>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <AuthProvider>
+        <Toaster position="top-right" />
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/verify-otp" element={<VerifyOtp />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Layout>
+                  <Dashboard />
+                </Layout>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
